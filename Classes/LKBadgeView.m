53c343ccafba64f6dd31c088dba2e08f7e156621
Julien CAMPY <julien@campy.fr>
2017-10-06 15:39:52 +0200

    Fix deprecated code

--- Classes/LKBadgeView.m
+++ Classes/LKBadgeView.m
@@ -116,16 +116,19 @@
 
 - (void)_adjustBadgeFrameWith
 {
-    CGSize suffixSize = [LK_BADGE_VIEW_TRUNCATED_SUFFIX sizeWithFont:self.font];
+    CGSize suffixSize = [LK_BADGE_VIEW_TRUNCATED_SUFFIX sizeWithAttributes:
+                         @{NSFontAttributeName: self.font}];
 
     CGFloat paddinWidth = LK_BADGE_VIEW_HORIZONTAL_PADDING*2;
-    CGSize size = [self.displayinText sizeWithFont:self.font];
+    CGSize size = [self.displayinText sizeWithAttributes:
+                   @{NSFontAttributeName: self.font}];
     badgeFrame_.size.width = size.width + paddinWidth;
     
     if (badgeFrame_.size.width > self.bounds.size.width) {
 
         while (1) {
-            size = [self.displayinText sizeWithFont:self.font];
+            size = [self.displayinText sizeWithAttributes:
+                    @{NSFontAttributeName: self.font}];
             badgeFrame_.size.width = size.width + paddinWidth;
             if (badgeFrame_.size.width+suffixSize.width > self.bounds.size.width) {
                 if ([self.displayinText length] > 1) {
@@ -260,17 +263,18 @@
     // draw text
     if (self.text != nil || [self.text length] > 0) {
         [self.textColor setFill];
-        CGSize size = [self.displayinText sizeWithFont:self.font];
+        CGSize size = [self.displayinText sizeWithAttributes:
+                       @{NSFontAttributeName: self.font}];
         CGPoint p = CGPointMake(bp.x + (badgeFrame_.size.width - size.width)/2.0 + textOffset_.width,
                                 bp.y + (badgeFrame_.size.height - size.height)/2.0 + textOffset_.height);
 
         if (self.shadowOfText) {
             CGContextSaveGState(context);
             CGContextSetShadowWithColor(context, self.shadowOffset, self.shadowBlur, self.shadowColor.CGColor);
-            [self.displayinText drawAtPoint:p withFont:self.font];
+            [self.displayinText drawAtPoint:p withAttributes:@{NSFontAttributeName: self.font}];
             CGContextRestoreGState(context);            
         } else {
-            [self.displayinText drawAtPoint:p withFont:self.font];
+            [self.displayinText drawAtPoint:p withAttributes:@{NSFontAttributeName: self.font}];
         }
     }
     
